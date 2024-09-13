197bd86876561b96adeb68dd5ff8f4c5e86280b7
OliverCoding <123648459+OliverDeltion@users.noreply.github.com>
2024-09-13 15:42:45 +0200

    Update Home.js

--- src/views/Home.js
+++ src/views/Home.js
@@ -28,21 +28,13 @@ export default function Home() {
       <div className="relative bg-white py-24 sm:py-32">
         <div className="flex flex-col lg:flex-row">
           {/* Filter component */}
-          <div className="lg:w-1/4 p-6">
+          <div className="lg:w-1/4 w-1/4 p-2">
             <Filter />
           </div>
-          
-          {/* Main content */}
-          <div className="flex-1 p-6 lg:p-8">
-            {/* Text positioned to top-right */}
-            <div className="absolute top-0 left-0 max-w-4xl">
-              <p className="mt-6 text-lg leading-8 text-gray-600 text-right">
-                {/* Add any additional text here if needed */}
-              </p>
-            </div>
+
 
             {/* People grid */}
-            <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-10 px-6 lg:px-8 xl:grid-cols-3">
+            <div className="mx-auto grid max-w-7xl gap-x-2 gap-y-10 px-2 lg:px-8 xl:grid-cols-3">
               <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-3 sm:gap-y-16 xl:col-span-3">
                 <Card />
                 <Card />
@@ -61,6 +53,5 @@ export default function Home() {
           </div>
         </div>
       </div>
-    </div>
   );
 }
