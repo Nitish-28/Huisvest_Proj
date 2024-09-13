d365724b993a0fcda541e048c1b99912203c7f5b
OliverCoding <123648459+OliverDeltion@users.noreply.github.com>
2024-09-13 11:41:05 +0200

    Header+login
    Header toegevoegd en tailwind gefixt van de login pagina

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,9 @@
 
-
+import Header from "../components/Header"
 export default function Login() {
     return (
       <>
-       
+       <Header> </Header>
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
