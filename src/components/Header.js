776802417668e62b4f5e89eebcacf3ad7dd27d51
OliverCoding <123648459+OliverDeltion@users.noreply.github.com>
2024-09-13 15:36:33 +0200

    homepage gemaakt

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,7 +44,6 @@ export default function Header() {
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img alt="" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" className="h-8 w-auto" />
           </a>
         </div>
         <div className="flex lg:hidden">
@@ -126,7 +125,6 @@ export default function Header() {
               <span className="sr-only">Your Company</span>
               <img
                 alt=""
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 className="h-8 w-auto"
               />
             </a>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./views/Home";
+import Home from './views/Home';
 import Dashboard from "./views/Dashboard";
 import Login from "./views/Login";
 import './index.css'
@@ -11,6 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="login" element={<Login />} />
         </Route>
