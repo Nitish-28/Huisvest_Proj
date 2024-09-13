1ab0d4fc11395ce5a9235a21b4b0001982c771cc
Nitish <Nitishgajadin6@gmail.com>
2024-09-13 15:11:47 +0200

    Login/Register
    begin login en register

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import Dashboard from "./views/Dashboard";
 import Login from "./views/Login";
+import Register from "./views/Register";
 import './index.css'
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
           <Route index element={<Home />} />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
         </Route>
       </Routes>
     </BrowserRouter>
