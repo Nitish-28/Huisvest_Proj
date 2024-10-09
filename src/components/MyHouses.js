d94ff5497588cb001933cf2947ac2ccbe01d3566
OliverCoding <123648459+OliverDeltion@users.noreply.github.com>
2024-10-09 11:22:07 +0200

    Dashboard fixes
    Dashboard card gemaakt

--- src/components/MyHouses.js
+++ src/components/MyHouses.js
@@ -1,11 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from '../components/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 export default function MyHouses() {
+  const [apiData, setApiData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios({
+          method: 'GET',
+          url: 'https://chrisouboter.com/api/content',
+        });
+        setApiData(response.data.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
     return (
-      <div class="grid grid-cols-3 gap-2">
-  <div>01</div>
-  <div>02</div>
-  <div>03</div>
-  <div>09</div>
-</div>
+      <div className="flex justify-center items-center h-64 gap-x-8 gap-y-2">
+        <FontAwesomeIcon icon={faSpinner} spin size="2x" />
+      </div>
     );
   }
-  
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div className="grid grid-cols-1 gap-2">
+      {apiData.length ? (
+        apiData.map((card) => (
+          <Card
+            type={card.type}
+            key={card.id}
+            title={card.address}
+            price={card.price}
+          />
+        ))
+      ) : (
+        <div>No houses found.</div>
+      )}
+    </div>
+  );
+}
