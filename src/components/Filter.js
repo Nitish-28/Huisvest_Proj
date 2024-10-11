4f98a5b918b7a35ca947ef4e6514084ce9ff05fd
OliverCoding <123648459+OliverDeltion@users.noreply.github.com>
2024-10-11 12:59:54 +0200

    fixes filter
    aaaaaaaa

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -39,11 +39,11 @@ export default function Filter() {
         </div>
       </Dialog>
 
-      <main className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <main className="mx-auto max-w-4xl px-2 sm:px-6 lg:px-8">
         <section aria-labelledby="filters-heading" className="pb-24 pt-6">
           <h2 id="filters-heading" className="sr-only">Filters</h2>
 
-          <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-1">
             <form className="hidden lg:block space-y-4 border-b border-gray-200 pb-6">
               {/* Subcategories */}
               <h3 className="font-medium">Subcategories</h3>
