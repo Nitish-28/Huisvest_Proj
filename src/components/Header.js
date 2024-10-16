b8cf537472b722facc10f08f6bf719c8062f6178
OliverCoding <123648459+OliverDeltion@users.noreply.github.com>
2024-10-16 13:25:33 +0200

    profile page fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,13 +10,14 @@ import { XMarkIcon } from "@heroicons/react/24/outline"; // Import XMarkIcon her
 import MainLogo from "./MainLogo";
 import { useToken } from "../ctx/TokenContext";
 import ApiConnection from "../components/ApiConnection";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  
-  const { token, logout } = useToken();
 
+  const { token, logout } = useToken();
   const [notifications, setNotifications] = useState([]);
+  const navigate = useNavigate(); // Initialize navigate
 
   useEffect(() => {
     const fetchData = async () => {
@@ -49,15 +50,15 @@ export default function Header() {
       >
         <div className="flex lg:flex-1 flex-2">
           <a href="/home" className="p-2">
-          <MainLogo text={true} />
+            <MainLogo text={true} />
           </a>
         </div>
         <a href="/dashboard" className="p-2 text-sm">
           Dashboard
-          </a>
-          <a href="/bids" className="p-2 text-sm">
+        </a>
+        <a href="/bids" className="p-2 text-sm">
           Your Bids
-          </a>
+        </a>
         <div className="hidden relative lg:flex lg:flex-1 lg:justify-end space-x-4">
           {token ? (
             <>
@@ -66,15 +67,17 @@ export default function Header() {
                 <Popover.Button className="flex text-base items-center rounded-lg px-3 py-2 font-semibold leading-7 bg-prim-green text-center p-4 transition duration-300 ease-in-out transform hover:bg-tert-blue hover:scale-105">
                   <HiBell className="size-6" />
                 </Popover.Button>
-                <Popover.Panel className="absolute right-0 mt-2 w-72 bg-white rounded-lg shadow-lg z-50" >
-                  <div className="p-4" >
+                <Popover.Panel className="absolute right-0 mt-2 w-72 bg-white rounded-lg shadow-lg z-50">
+                  <div className="p-4">
                     <h3 className="font-bold text-gray-700">Notifications</h3>
                     <ul className="mt-2">
                       {notifications.length > 0 ? (
                         notifications.map((notification) => (
                           <li
                             key={notification.id}
-                            className={`py-2 text-sm  p-2 border-b border-gray-200 text-black last:border-0 ${notification.read ? "" : "bg-blue-200"}`}
+                            className={`py-2 text-sm  p-2 border-b border-gray-200 text-black last:border-0 ${
+                              notification.read ? "" : "bg-blue-200"
+                            }`}
                           >
                             {notification.message}
                           </li>
@@ -90,7 +93,7 @@ export default function Header() {
               </Popover>
 
               {/* User Popover */}
-              <Popover className="relative"  style={{ zIndex: 1500 }}>
+              <Popover className="relative" style={{ zIndex: 1500 }}>
                 <Popover.Button className="flex text-base items-center rounded-lg px-3 py-2 font-semibold leading-7 bg-prim-green text-center p-4 transition duration-300 ease-in-out transform hover:bg-tert-blue hover:scale-105">
                   <HiUser className="size-6" />
                 </Popover.Button>
@@ -98,29 +101,27 @@ export default function Header() {
                   <div className="p-4">
                     <h3 className="font-bold text-gray-700">User Settings</h3>
                     <ul className="mt-2">
-                    <button
-                      className="flex my-2 text-base items-center rounded-lg px-3 py-2 font-semibold leading-7 hover:bg-prim-green  text-black text-center p-4 transition duration-300 ease-in-out transform hover:bg-tert-blue hover:scale-105"
-                    >
-                      Profile 
-                    </button>
                       <button
-                      className="flex my-2 text-base items-center rounded-lg px-3 py-2 font-semibold leading-7 hover:bg-prim-green text-black text-center p-4 transition duration-300 ease-in-out transform hover:bg-tert-blue hover:scale-105"
-                    >
-                      Settings
-                    </button>
+                        onClick={() => navigate("/profile")} // Navigate to profile on click
+                        className="flex my-2 text-base items-center rounded-lg px-3 py-2 font-semibold leading-7 hover:bg-prim-green text-black text-center p-4 transition duration-300 ease-in-out transform hover:bg-tert-blue hover:scale-105"
+                      >
+                        Profile
+                      </button>
                       <button
-                      onClick={logout}
-                      className="flex my-2 text-base items-center rounded-lg px-3 py-2 font-semibold leading-7 hover:bg-prim-green text-black  text-center p-4 transition duration-300 ease-in-out transform hover:bg-tert-blue hover:scale-105"
-                    >
-                      Log out <span aria-hidden="true">&rarr;</span>
-                    </button>{" "}
+                        className="flex my-2 text-base items-center rounded-lg px-3 py-2 font-semibold leading-7 hover:bg-prim-green text-black text-center p-4 transition duration-300 ease-in-out transform hover:bg-tert-blue hover:scale-105"
+                      >
+                        Settings
+                      </button>
+                      <button
+                        onClick={logout}
+                        className="flex my-2 text-base items-center rounded-lg px-3 py-2 font-semibold leading-7 hover:bg-prim-green text-black  text-center p-4 transition duration-300 ease-in-out transform hover:bg-tert-blue hover:scale-105"
+                      >
+                        Log out <span aria-hidden="true">&rarr;</span>
+                      </button>
                     </ul>
                   </div>
                 </Popover.Panel>
               </Popover>
-
-              {/* Logout Button */}
-              
             </>
           ) : (
             <a
